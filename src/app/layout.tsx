53da338cba38a3ec5b06a641627c0789104b2965
Daniel Cintora <dcintora.ing@gmail.com>
2026-05-25 03:32:23 +0000

    necesito que metas estos codigos en mis ventanas para analizarlos con go

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase';
 import { AuthProvider } from '@/context/auth-context';
 import { ProtectedRoute } from '@/components/protected-route';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'SyncStock Inventario',
@@ -26,8 +27,29 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M2Z8J67Q');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body className="font-body antialiased bg-background selection:bg-primary/30 selection:text-primary-foreground">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M2Z8J67Q"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <FirebaseClientProvider>
           <AuthProvider>
             <ProtectedRoute>
